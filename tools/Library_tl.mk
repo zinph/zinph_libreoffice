3395c3ed22519c62b091a5065e03862bda587f20
László Németh <laszlo.nemeth@collabora.com>
2015-11-06 01:14:31 +0100

    Revert "invalid array index when pCurrent pointer is incremented"
    This reverts commit b35c38c6e44b0df0fc2c5a3983ecd7547b964691.
    
    Revert "Fast array sum: aligned load, process 8 doubles per loop"
    
    This reverts commit f814b00bc908c5498156194f45bf8f9c0b8268ac.
    
    Revert "arraysumfunctor: fast sum a double array, use for SUM() in Calc"
    
    This reverts commit e59e6c572f3e7531800b396f7e4ad5f52f98d987.

--- tools/Library_tl.mk
+++ tools/Library_tl.mk
@@ -69,7 +69,6 @@ $(eval $(call gb_Library_add_exception_objects,tl,\
     tools/source/memtools/multisel \
     tools/source/memtools/unqidx \
     tools/source/misc/appendunixshellword \
-    tools/source/misc/cpuid \
     tools/source/misc/extendapplicationenvironment \
     tools/source/misc/getprocessworkingdir \
     tools/source/misc/solarmutex \

bennhatton/libreoffice

tdf#88206 pre-compiled headers too
Change-Id: I4c88d986292eac9f772336f07a5c0da32db5e3b8

drawinglayer/inc/pch/precompiled_drawinglayer.hxx
@@ -83,7 +83,7 @@
 #include <comphelper/unique_disposing_ptr.hxx>
 #include <comphelper/string.hxx>
 #include <cppuhelper/factory.hxx>
-#include <cppuhelper/implbase2.hxx>
+#include <cppuhelper/implbase.hxx>
 #include <cppuhelper/supportsservice.hxx>
 #include <cstdio>
 #include <i18nlangtag/languagetag.hxx>

zinph/zinph_libreoffice

drawinglayer/inc/pch/precompiled_drawinglayer.hxx
@@ -114,7 +114,7 @@
 #include <com/sun/star/uno/Reference.hxx>
 #include <comphelper/broadcasthelper.hxx>
 #include <comphelper/processfactory.hxx>
-#include <cppuhelper/compbase1.hxx>
+#include <cppuhelper/compbase.hxx>
 #include <cppuhelper/interfacecontainer.h>
 #include <cppuhelper/weakref.hxx>
 #include <i18nlangtag/i18nlangtagdllapi.h>